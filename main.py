65bcb6c8dad14a8f373d34cbbd741b206ef72957
hei hei OK OK! <98307235+laisammy@users.noreply.github.com>
2026-03-06 22:03:02 +0000

    Added flappy bird animations

--- main.py
+++ main.py
@@ -17,15 +17,47 @@ ground = pygame.image.load("assets/ground.png")
 groundScroll = 0
 scrollSpeed = 4
 
-clock = pygame.time.Clock
+clock = pygame.time.Clock()
 fps = 60
 
+class Bird(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.images = []
+        self.index = 0
+        self.counter = 0
+        for num in range(1, 4): # Append sprite frames to images
+            img = pygame.image.load(f"assets/bird{num}.png")
+            self.images.append(img)
+        self.image = self.images[self.index]
+        self.rect = self.image.get_rect()
+        self.rect.center = [x, y]
+
+    def update(self):
+        self.counter += 1
+        flapCD = 5
+
+        if self.counter > flapCD:
+            self.counter = 0
+            self.index += 1
+            if self.index >= len(self.images): 
+                self.index = 0
+        self.image = self.images[self.index]
+
+birdGroup = pygame.sprite.Group()
+
+flappy = Bird(100, int(screenHeight/2))
+
+birdGroup.add(flappy)
 
 run = True
 while run:
     clock.tick(fps) # Sets game to 60fps
 
     screen.blit(bg, (0,0)) # Draw background
+
+    birdGroup.draw(screen)
+    birdGroup.update()
     
     screen.blit(ground, (groundScroll, 768)) # Draw and scroll ground
     groundScroll -= scrollSpeed
